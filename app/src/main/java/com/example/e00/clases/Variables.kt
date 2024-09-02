f958de0d58faa9c01228857fd61c429379e49de8
KryzNov <kryzglz02@gmail.com>
2024-09-01 21:03:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/e00/clases/Variables.kt
@@ -0,0 +1,39 @@
+package com.example.e00.clases
+
+class  Variables {
+
+
+    fun main() {
+
+        val age: Int = 20
+        val long_number: Long = 984311111111111
+        val temperature: Float = 27.123f
+        val weight: Double = 60.4
+
+        val gender: Char = 'M'
+        val name: String = "Cristian Gonzalez"
+
+        val isGreater: Boolean = false
+
+
+        val names = arrayOf("Erik", "Silvia", "Hector", "Gabriela")
+
+        println(age)
+        println("Bienvenido $name")
+
+    }
+
+
+}
+
+    //fun add():Int{
+        //val x:Int = 5
+        //val y:Int = 10
+
+      //  return x + y
+    //}
+
+    //fun product(x: Int, y: Int): Int {
+    //    return x + y
+    //}
+
